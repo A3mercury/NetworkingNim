842b450286f465896fc4ac16af0d7d6cb7b3d816
A3mercury <aandrew1@harding.edu>
2015-04-24 01:27:19 -0500

    Some more GUI stuff

---
+++ NimCSharp/NimCSharp/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NimCSharp
+{
+    /// <summary>
+    /// This Window is just the container for the pages inside of it.
+    /// As soon as the Window is initialized it will navigate it's frame
+    /// to the MenuPage
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static Frame _MainFrame;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _MainFrame = _MainWindowFrame;
+          
+            _MainWindowFrame.Navigate(new MenuPage());
+        }
+    }
+}
---
+++ NimCSharp/NimCSharp/MenuPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NimCSharp
+{
+    /// <summary>
+    /// Interaction logic for MenuPage.xaml
+    /// </summary>
+    public partial class MenuPage : Page
+    {
+        public static string Username;
+
+        public MenuPage()
+        {
+            InitializeComponent();
+
+            Username = UsernameBox.Text;
+        }
+
+        private void FindPlayers(object sender, RoutedEventArgs e)
+        {
+            if(UsernameBox.Text != "" && UsernameBox.Text != "Username")
+                MainWindow._MainFrame.Navigate(new OpponentList());
+        }
+    }
+}
